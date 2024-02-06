7b81c4144ee7972e27587872a82982ee087f6031
Anil <karapaanilkumar005@gmail.com>
2024-02-05 21:04:45 -0500

    components set up

--- modern_portfolio/src/main.jsx
+++ modern_portfolio/src/main.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Home from "./pages/home.jsx";
+import About from "./components/about/About";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import Experience from "./components/experience/Experience";
+import Talks from "./components/talks/Talks";
+import Footer from "./components/footer/Footer";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Home />
+    <Header />
+    <Nav />
+    <About />
+    <Experience />
+    <Talks />
+    <Footer />
   </React.StrictMode>
 );
