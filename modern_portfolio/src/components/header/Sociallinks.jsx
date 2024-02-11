27e26ac0b3a89125c07af788b068ef4766d04587
Anil <karapaanilkumar005@gmail.com>
2024-02-10 22:21:21 -0500

    updated header styling and social links

--- modern_portfolio/src/components/header/Sociallinks.jsx
+++ modern_portfolio/src/components/header/Sociallinks.jsx
@@ -5,16 +5,39 @@ import { MdOutlineWeb } from "react-icons/md";
 
 const Sociallinks = () => {
   return (
+<<<<<<< Updated upstream
     <div className="social_links">
       <a href="#Linkedin" target="_blank">
         <FaLinkedin />
       </a>
       <a href="#Linkedin" target="_blank">
+=======
+    <div className="social_links_container">
+      <div className="social_links">
+        {/* Dynamically updating values */}
+        {socialDetails.map((socialDetail, index) => (
+          <a
+            href={socialDetail.href}
+            key={index}
+            title={socialDetail.title}
+            target="_blank"
+          >
+            {socialDetail.icon}
+          </a>
+        ))}
+
+        {/* <a href="#Linkedin" target="_blank">
+>>>>>>> Stashed changes
         <FaGithub />
       </a>
       <a href="#Linkedin" target="_blank">
         <MdOutlineWeb />
+<<<<<<< Updated upstream
       </a>
+=======
+      </a> */}
+      </div>
+>>>>>>> Stashed changes
     </div>
   );
 };
