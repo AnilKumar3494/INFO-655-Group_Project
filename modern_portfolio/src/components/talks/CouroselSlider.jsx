fe5f35fd5f5d8cfb732059b1936a3954d151dc31
Anil <karapaanilkumar005@gmail.com>
2024-02-20 19:50:10 -0500

    small updates

--- modern_portfolio/src/components/talks/CouroselSlider.jsx
+++ modern_portfolio/src/components/talks/CouroselSlider.jsx
@@ -21,33 +21,28 @@ const CouroselSlider = ({ slideHeadings }) => {
       if (prevIndex === slideHeadings.length - 1) {
         return 0;
       }
+
       return prevIndex + 1;
     });
   }
 
   return (
     <div className="courosel_wrap">
-      <div className="individual_talk">
-        <h3>{slideHeadings[talkIndex].title}</h3>
-        <p>{slideHeadings[talkIndex].content}</p>
-        <button
-          className="slider_btn"
-          style={{ left: 0 }}
-          onClick={showPrevImg}
-        >
-          <FaAngleLeft />
-        </button>
-        <button
-          className="slider_btn"
-          style={{ right: 0 }}
-          onClick={showNextImg}
-        >
-          <FaAngleRight />
-        </button>
-        <a href={slideHeadings[talkIndex].url} target="_blank">
-          Know more
-        </a>
+      <div>
+        <div className="individual_talk">
+          <h3>{slideHeadings[talkIndex].title}</h3>
+          <p>{slideHeadings[talkIndex].content}</p>
+        </div>
       </div>
+      <button className="slider_btn" style={{ left: 0 }} onClick={showPrevImg}>
+        <FaAngleLeft />
+      </button>
+      <button className="slider_btn" style={{ right: 0 }} onClick={showNextImg}>
+        <FaAngleRight />
+      </button>
+      {/* <a href={slideHeadings[talkIndex].url} target="_blank">
+          Know more
+        </a> */}
     </div>
   );
 };
