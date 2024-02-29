e5b2770caa5a996369bf85fb7286ab5fd5eaeb9a
yuwei-3206 <yw829@drexel.edu>
2024-02-29 15:37:01 -0500

    Set the icon as active when scrolling to the section

--- modern_portfolio/src/components/nav/Nav.jsx
+++ modern_portfolio/src/components/nav/Nav.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./nav.css";
 
 import { FaHome, FaUser, FaFileCode, FaBlog } from "react-icons/fa";
@@ -6,34 +6,40 @@ import { FaHome, FaUser, FaFileCode, FaBlog } from "react-icons/fa";
 import { MdOutlineMessage } from "react-icons/md";
 
 const Nav = () => {
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [activeNav, setActiveNav] = useState("#");
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting && entry.intersectionRatio >= 0.5) {
+            setActiveNav(`#${entry.target.id}`);
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
+
+    const sections = document.querySelectorAll("section[id]");
+    sections.forEach((section) => observer.observe(section));
+
+    return () => {
+      sections.forEach((section) => observer.unobserve(section));
+    };
+  }, []);
+
+  /**const [activeIndex, setActiveIndex] = useState(0);*/
 
   const navItems = [
-    { icon: <FaHome />, toolTip: "Home", id: "#", url: "#" },
-    { icon: <FaUser />, toolTip: "User", id: "#", url: "#" },
-    {
-      icon: <FaBlog />,
-      toolTip: "Message",
-      id: "#",
-      url: "#",
-    },
-    {
-      icon: <FaFileCode />,
-      toolTip: "Experience",
-      id: "#experience",
-      url: "#",
-    },
-    {
-      icon: <MdOutlineMessage />,
-      toolTip: "Message",
-      id: "#contact",
-      url: "#",
-    },
+    { icon: <FaHome />, toolTip: "Home", id: "#header", url: "#" },
+    { icon: <FaUser />, toolTip: "User", id: "#about", url: "#" },
+    { icon: <FaBlog />, toolTip: "Message", id: "#services", url: "#"},
+    { icon: <FaFileCode />, toolTip: "Experience", id: "#projects", url: "#"},
+    { icon: <MdOutlineMessage />, toolTip: "Message", id: "#contact", url: "#"}
   ];
 
-  function activeClass() {
+  /*function activeClass() {
     return "active";
-  }
+  }*/
 
   return (
     <nav>
@@ -43,7 +49,8 @@ const Nav = () => {
             href={item.id}
             title={item.toolTip}
             onClick={() => setActiveIndex(itemIndex)}
-            className={itemIndex === activeIndex ? "active" : ""}
+            className={activeNav === item.id ? "active" : ""}
+            /*className={itemIndex === activeIndex ? "active" : ""}*/
           >
             {item.icon}
           </a>
